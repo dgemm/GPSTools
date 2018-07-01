763f1e692db649af6d25f8699e81a81a75a5eb8d
Jmaie <njl@??>
2018-07-01 13:56:26 -0700

    Read large files incrementally

--- GoogleLocationHistory/GoogleToKMZ.py
+++ GoogleLocationHistory/GoogleToKMZ.py
@@ -9,11 +9,12 @@ import zipfile
 try:
   import simplejson as json
 except ImportError:
+  print 'Using the slow json parser'
   import json
 
 
 # Files
-infile = 'Takeout/Location History/LocationHistory.json'
+infile = 'Takeout/Location History/Location History.json'
 outfile = 'out.kmz'
 
 
@@ -27,8 +28,62 @@ def parseloc(location):
     'lng': 0.0000001*location['longitudeE7'],
     'time': 0.001*int(location['timestampMs']),
   }
-  
+
   return result
+
+
+class LocationDataFile(object):
+  def __init__(self, fname):
+    self.f = open(infile, 'rb')
+    self.f.seek(0, 2)
+    self.f_size = self.f.tell()
+    self.rewind()
+
+  def rewind(self):
+    self.f.seek(1)  # Past the root level "{" character
+    self.progress = 0
+
+  def print_progress(self):
+    progress = int(100*float(self.f.tell())/self.f_size)
+
+    if progress != self.progress:
+      print '%d%%' % progress
+      self.progress = progress
+
+  def getchar(self):
+    return self.f.read(1)
+
+
+def get_next_item(f):
+  chunk = ''
+  curlies = 0
+
+  while True:
+    f.print_progress()
+    c = f.getchar()
+
+    if not c:
+      raise StopIteration
+
+    if len(chunk) == 0 and c != '{':
+      # Skip until the first "{"
+      continue
+
+    # Count opening and closing curlies to find the end of object
+    if c == '{':
+      curlies += 1
+    elif c == '}':
+      curlies -= 1
+
+    chunk += c
+
+    if curlies == 0:
+      yield json.loads(chunk)
+
+      # Reset chunk
+      chunk = ''
+      curlies = 0
+  #
 
 
 # Add an endpoint (stop) to the map.
@@ -36,20 +91,20 @@ def addEndpoint(doc, PositionsFolder, lat, lng):
   # Add the endpoint
   EndPlacemark = doc.createElement('Placemark')
   PositionsFolder.appendChild(EndPlacemark)
-  
+
   Point = doc.createElement('Point')
   EndPlacemark.appendChild(Point)
-  
+
   extrude = doc.createElement('extrude')
   txt = doc.createTextNode('1')
   extrude.appendChild(txt)
   Point.appendChild(extrude)
-  
+
   altitudeMode = doc.createElement('altitudeMode')
   txt = doc.createTextNode('relativeToGround')
   altitudeMode.appendChild(txt)
   Point.appendChild(altitudeMode)
-  
+
   coordinates = doc.createElement('coordinates')
   txt = doc.createTextNode('%f,%f,0' % (lng, lat))
   coordinates.appendChild(txt)
@@ -62,60 +117,61 @@ def main():
   this_dir = os.path.dirname(os.path.abspath(__file__))
   datafile = os.path.join(this_dir, infile)
   absoutfile = os.path.join(this_dir, outfile)
-  
+
   # Start the KML document
   doc = xml.dom.minidom.Document()
   kml = doc.createElement('kml')
   kml.setAttribute('xmlns', 'http://www.opengis.net/kml/2.2')
   doc.appendChild(kml)
-  
+
   Document = doc.createElement('Document')
   kml.appendChild(Document)
-  
+
   name = doc.createElement('name')
   txt = doc.createTextNode('Track Name')
   name.appendChild(txt)
   Document.appendChild(name)
-  
+
   description = doc.createElement('description')
   txt = doc.createTextNode('Track Description')
   description.appendChild(txt)
   Document.appendChild(description)
-  
+
   PositionsFolder = doc.createElement('Folder')
   PositionsFolder.setAttribute('id', 'Positions')
   Document.appendChild(PositionsFolder)
-  
+
   fopen = doc.createElement('open')
   txt = doc.createTextNode('1')
   fopen.appendChild(txt)
   PositionsFolder.appendChild(fopen)
-  
-  # Read data file
-  with open(datafile, 'r') as f:
-    data = json.loads(f.read())
-  
+
+  # Open data file
+  f = LocationDataFile(datafile)
+
   # Make a list of points where we were stopped
   stillstops = []
-  
-  for item in data['locations']:
+
+  print 'Finding stops'
+
+  for item in get_next_item(f):
     try:
       acts = item['activitys']
     except KeyError:
       acts = None
     except IndexError:
       acts = None
-    
+
     if acts:
       for act in acts:
         for a in act['activities']:
           if (a['type'] == 'still') and (a['confidence'] == 100):
            stillstops.append(parseloc(item))
            break
-  
+
   # Write the stops to XML
   prev_stop = None
-  
+
   for stop in stillstops:
     if not stop:
       continue
@@ -127,20 +183,23 @@ def main():
 
       if d2 < MinLineThresh:
         continue
-    
+
     addEndpoint(doc, PositionsFolder, stop['lat'], stop['lng'])
     prev_stop = stop
-  
+
   # Path coordinate pairs
   coordslist = []
   coords = []
-  
+
   prev_point = {
     'lat': 0.0,
     'lng': 0.0,
   }
-  
-  for item in data['locations']:
+
+  print 'Finding points'
+  f.rewind()
+
+  for item in get_next_item(f):
     point = parseloc(item)
 
     # Squared distance between this point and previous
@@ -150,36 +209,36 @@ def main():
     if prev_point and (d2 > MaxLineThresh):
       coordslist.append(coords)
       coords = []
-    
+
     # Enforce minimum distance between points
     if prev_point and (d2 < MinLineThresh):
       continue
-    
+
     # Add this point
     coords.append('%f,%f' % (point['lng'], point['lat']))
     prev_point = point
-  
+
   # Add the last set of coords
   coordslist.append(coords)
   coords = []
-  
+
   LinesFolder = doc.createElement('Folder')
   LinesFolder.setAttribute('id', 'Lines')
   Document.appendChild(LinesFolder)
-  
+
   fopen = doc.createElement('open')
   txt = doc.createTextNode('1')
   fopen.appendChild(txt)
   LinesFolder.appendChild(fopen)
-  
+
   Placemark = doc.createElement('Placemark')
   LinesFolder.appendChild(Placemark)
-  
+
   pname = doc.createElement('name')
   txt = doc.createTextNode('GPS Track')
   pname.appendChild(txt)
   Placemark.appendChild(pname)
-  
+
   Style = doc.createElement('Style')
   Placemark.appendChild(Style)
   LineStyle = doc.createElement('LineStyle')
@@ -192,38 +251,38 @@ def main():
   txt = doc.createTextNode('3')
   width.appendChild(txt)
   LineStyle.appendChild(width)
-  
+
   MultiGeometry = doc.createElement('MultiGeometry')
   Placemark.appendChild(MultiGeometry)
-  
+
   for coords in coordslist:
     # Skip single point lines
     if len(coords) < 2:
       continue
-    
+
     LineString = doc.createElement('LineString')
     MultiGeometry.appendChild(LineString)
-    
+
     extrude = doc.createElement('extrude')
     txt = doc.createTextNode('1')
     extrude.appendChild(txt)
     LineString.appendChild(extrude)
-    
+
     tessellate = doc.createElement('tessellate')
     txt = doc.createTextNode('1')
     tessellate.appendChild(txt)
     LineString.appendChild(tessellate)
-    
+
     altitudeMode = doc.createElement('altitudeMode')
     txt = doc.createTextNode('RelativeToGround')
     altitudeMode.appendChild(txt)
     LineString.appendChild(altitudeMode)
-    
+
     coordinates = doc.createElement('coordinates')
     txt = doc.createTextNode(' '.join(coords))
     coordinates.appendChild(txt)
     LineString.appendChild(coordinates)
-  
+
   # Write KMZ file
   with zipfile.ZipFile(absoutfile, 'w', zipfile.ZIP_DEFLATED) as f:
     print
@@ -234,7 +293,7 @@ def main():
 # Do the stuff
 if __name__ == '__main__':
   main()
-  
+
   if os.name == 'nt':
     os.system('PAUSE')
 
